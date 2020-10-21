64a89e0a86e467bc346bdcf6d6a5455d7d5b600a
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-10-20 17:48:19 -0700

    Getting the steps is working correctly

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -8,8 +8,17 @@ function findById(id) {
     return database.first('*').from("schemes").where('id', id);
 }
 
+function findSteps(userId) {
+    return database("steps as St")
+    .join('schemes as Sc', 'St.scheme_id', 'Sc.id')
+    .where("St.scheme_id", userId)
+    .select('St.id', 'Sc.scheme_name', 'St.step_number', 'St.instructions')
+    .orderBy('St.step_number');
+}
+
 module.exports = {
     find,
     findById,
+    findSteps,
 
 }
